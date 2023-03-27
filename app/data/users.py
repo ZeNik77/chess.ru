aa73a71f33e88b11f82b34bc133870880221d686
Kalix228 <seniaveretenov07@mail.ru>
2023-03-27 21:34:03 +0200

    фикс+рейтинг

--- app/data/users.py
+++ app/data/users.py
@@ -10,6 +10,7 @@ class User(SqlAlchemyBase):
                            primary_key=True, autoincrement=True)
     glob_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 unique=True)
+    rating = sqlalchemy.Column(sqlalchemy.Integer)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
