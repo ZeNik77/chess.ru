c4a5ee84e8d9c24dad2286dff6bc897bfb10329b
ZeNik77 <42634950+ZeNik77@users.noreply.github.com>
2023-04-01 07:38:04 +0200

    с праздником

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-from .data import db_session
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-db_session.global_init("app/db/main.db")
-from app import routes
--- app/data/__all_models.py
+++
@@ -1,2 +0,0 @@
-from . import rooms
-from . import users
